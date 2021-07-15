c7afd7e35fc9d1e111885e2c7edf2a5422a853a0
Anastasia <nastia_kim_96@mail.ru>
2021-07-15 17:25:15 +1000

    init_project

---
+++ server/app.js
@@ -0,0 +1,43 @@
+require("dotenv").config();
+const express = require("express");
+const session = require("express-session");
+const morgan = require('morgan')
+const cors = require("cors");
+const MongoStore = require("connect-mongo");
+const { dbConnectionURL, connect } = require("./db/config/config");
+
+const app = express();
+
+const { PORT, COOKIE_SECRET, COOKIE_NAME } = process.env;
+
+connect();
+
+app.set("cookieName", COOKIE_NAME);
+
+app.use(
+  cors({
+    origin: true,
+    credentials: true,
+  })
+);
+app.use(express.json());
+app.use(
+  session({
+    name: app.get("cookieName"),
+    secret: COOKIE_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    store: MongoStore.create({
+      mongoUrl: dbConnectionURL,
+    }),
+    cookie: {
+      secure: false,
+      httpOnly: true,
+      maxAge: 1e3 * 86400,
+    },
+  })
+);
+
+app.listen(PORT, () => {
+  console.log("Server has been started on PORT ", PORT);
+});
---
+++ server/db/config/config.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+
+const options = {
+  useNewUrlParser: true, // говорим mongoose, что строка подключения будет в новом формате (новый формат должен обязательно содеражт порт)
+  useFindAndModify: false, // заставляем методы findOneAndUpdate() и findOneAndRemove() использовать нативный (т.е предоставленный самой mongodb) метод findOneAndUpdate() вместо findAndModify()
+  useCreateIndex: true, // Заставляем mongoose работать с функцией createIndex() драйвера mongodb вместо ensureIndex(). Так как последний помечен драйвером mongodb, как устаревший
+  useUnifiedTopology: true, // заставляем mongoose использование новый механизм управления подключением драйвера mongodb.
+  poolSize: 10, // максимальное количество сокетов, которые драйвер MongoDB будет держать открытыми для этого соединения
+  bufferMaxEntries: 0, // говорим mongoose перестать выполнять любые операции с базой данных, после того как произодет отключение от последней.
+  // В противном случае mongoose пытается дождаться восстановления соездинения, для завершения  операций
+};
+
+const { DB_HOST, DB_NAME, DB_PORT } = process.env;
+
+const dbConnectionURL = `mongodb://${DB_HOST}:${DB_PORT}/${DB_NAME}`;
+
+function connect() {
+  mongoose.connect(dbConnectionURL, options, (err) => {
+    if (err) console.error("ERROR WITH DB");
+
+    console.log("Connect to DB");
+  });
+}
+
+function disconnect() {
+  mongoose.disconnect();
+}
+
+module.exports = { connect, disconnect, dbConnectionURL };

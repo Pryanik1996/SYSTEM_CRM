b27ff4b64358263f124791c74da4fadbd9ad37fd
Kseniya <yabuzarovakm@gmail.com>
2021-07-18 12:31:54 +0300

    add table  allClients

--- client/src/components/AllClients/AllClients.js
+++ client/src/components/AllClients/AllClients.js
@@ -1,6 +1,54 @@
-import { useEffect } from "react";
+import React from "react";
+import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { getClients } from "../../redux/actions/clients.action";
+import { makeStyles } from "@material-ui/core/styles";
+import Paper from "@material-ui/core/Paper";
+import Table from "@material-ui/core/Table";
+import TableBody from "@material-ui/core/TableBody";
+import TableCell from "@material-ui/core/TableCell";
+import TableContainer from "@material-ui/core/TableContainer";
+import TableHead from "@material-ui/core/TableHead";
+import TablePagination from "@material-ui/core/TablePagination";
+import TableRow from "@material-ui/core/TableRow";
+
+const columns = [
+  { id: "surname", label: "Фамилия", minWidth: 170 },
+  { id: "name", label: "Имя", minWidth: 100 },
+  {
+    id: "patronymic",
+    label: "Отчество",
+    minWidth: 170,
+    // align: "right",
+  },
+  {
+    id: "email",
+    label: "Email",
+    minWidth: 170,
+  },
+  {
+    id: "phone",
+    label: "Телефон",
+    minWidth: 170,
+  },
+  {
+    id: "orders",
+    label: "Заказы",
+    minWidth: 170,
+  },
+];
+// function createData(surname, name, patronymic, email, phone, orders) {
+//   return { surname, name, patronymic, email, phone, orders };
+// }
+
+const useStyles = makeStyles({
+  root: {
+    width: "100%",
+  },
+  container: {
+    maxHeight: 440,
+  },
+});
 
 export default function AllClients() {
   const {
@@ -14,6 +62,21 @@ export default function AllClients() {
     dispatch(getClients());
   }, [dispatch]);
 
+  const classes = useStyles();
+  const [page, setPage] = useState(0);
+  const [rowsPerPage, setRowsPerPage] = useState(10);
+
+  const handleChangePage = (event, newPage) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (event) => {
+    setRowsPerPage(+event.target.value);
+    setPage(0);
+  };
+
+  console.log(clients);
+
   return (
     <div>
       <h1>Все клиенты</h1>
@@ -26,15 +89,72 @@ export default function AllClients() {
           {clients.length === 0 ? (
             <p>Клиентов нет</p>
           ) : (
-            <ul>
-              {clients?.map((cl) => (
-                <div>
-                  <a key={cl._id} href="/">
-                    {cl.name} {cl.surname} {cl.email} {cl.phone}
-                  </a>
-                </div>
-              ))}
-            </ul>
+            // <ul>
+            //   {clients?.map((cl) => (
+            //     <div>
+            //       <a key={cl._id} href="/">
+            //         {cl.name} {cl.surname} {cl.email} {cl.phone}
+            //       </a>
+            //     </div>
+            //   ))}
+            // </ul>
+
+            <Paper className={classes.root}>
+              <TableContainer className={classes.container}>
+                <Table stickyHeader aria-label="sticky table">
+                  <TableHead>
+                    <TableRow>
+                      {columns.map((column) => (
+                        <TableCell
+                          key={column.id}
+                          align={column.align}
+                          style={{ minWidth: column.minWidth }}
+                        >
+                          {column.label}
+                        </TableCell>
+                      ))}
+                    </TableRow>
+                  </TableHead>
+                  <TableBody>
+                    {clients
+                      .slice(
+                        page * rowsPerPage,
+                        page * rowsPerPage + rowsPerPage
+                      )
+                      .map((client) => {
+                        return (
+                          <TableRow
+                            hover
+                            role="checkbox"
+                            tabIndex={-1}
+                            key={client._id}
+                          >
+                            {columns.map((column) => {
+                              const value = client[column.id];
+                              return (
+                                <TableCell key={column.id} align={column.align}>
+                                  {column.format && typeof value === "number"
+                                    ? column.format(value)
+                                    : value}
+                                </TableCell>
+                              );
+                            })}
+                          </TableRow>
+                        );
+                      })}
+                  </TableBody>
+                </Table>
+              </TableContainer>
+              <TablePagination
+                rowsPerPageOptions={[10, 25, 100]}
+                component="div"
+                count={clients.length}
+                rowsPerPage={rowsPerPage}
+                page={page}
+                onPageChange={handleChangePage}
+                onRowsPerPageChange={handleChangeRowsPerPage}
+              />
+            </Paper>
           )}
         </>
       )}

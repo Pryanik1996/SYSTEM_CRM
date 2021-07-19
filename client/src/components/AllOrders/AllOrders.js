d57cbbba3be4a9bee65977ec572f2b5819de5a7d
Kseniya <yabuzarovakm@gmail.com>
2021-07-19 14:54:37 +0300

    ad dinfo on orders page

--- client/src/components/AllOrders/AllOrders.js
+++ client/src/components/AllOrders/AllOrders.js
@@ -3,7 +3,7 @@ import { useDispatch, useSelector } from "react-redux";
 import { getOrders } from "../../redux/actions/order.action";
 import { Link } from "react-router-dom";
 import { ORDER_ADD } from "../../redux/types";
-import {useState} from "react"
+import { useState } from "react";
 
 export default function AllOrders() {
   const {
@@ -19,58 +19,127 @@ export default function AllOrders() {
 
   //============== SEARCH
 
-  let keyCl = ['number', 'client', 'status', 'creator']
+  let keyCl = ["number", "client", "status", "creator"];
 
-  function translit(word){
-    var answer = '';
+  function translit(word) {
+    var answer = "";
     var converter = {
-      'а': 'a',    'б': 'b',    'в': 'v',    'г': 'g',    'д': 'd',
-      'е': 'e',    'ё': 'e',    'ж': 'zh',   'з': 'z',    'и': 'i',
-      'й': 'y',    'к': 'k',    'л': 'l',    'м': 'm',    'н': 'n',
-      'о': 'o',    'п': 'p',    'р': 'r',    'с': 's',    'т': 't',
-      'у': 'u',    'ф': 'f',    'х': 'h',    'ц': 'c',    'ч': 'ch',
-      'ш': 'sh',   'щ': 'sch',  'ь': '',     'ы': 'y',    'ъ': '',
-      'э': 'e',    'ю': 'yu',   'я': 'ya',
-   
-      'А': 'A',    'Б': 'B',    'В': 'V',    'Г': 'G',    'Д': 'D',
-      'Е': 'E',    'Ё': 'E',    'Ж': 'Zh',   'З': 'Z',    'И': 'I',
-      'Й': 'Y',    'К': 'K',    'Л': 'L',    'М': 'M',    'Н': 'N',
-      'О': 'O',    'П': 'P',    'Р': 'R',    'С': 'S',    'Т': 'T',
-      'У': 'U',    'Ф': 'F',    'Х': 'H',    'Ц': 'C',    'Ч': 'Ch',
-      'Ш': 'Sh',   'Щ': 'Sch',  'Ь': '',     'Ы': 'Y',    'Ъ': '',
-      'Э': 'E',    'Ю': 'Yu',   'Я': 'Ya'
+      а: "a",
+      б: "b",
+      в: "v",
+      г: "g",
+      д: "d",
+      е: "e",
+      ё: "e",
+      ж: "zh",
+      з: "z",
+      и: "i",
+      й: "y",
+      к: "k",
+      л: "l",
+      м: "m",
+      н: "n",
+      о: "o",
+      п: "p",
+      р: "r",
+      с: "s",
+      т: "t",
+      у: "u",
+      ф: "f",
+      х: "h",
+      ц: "c",
+      ч: "ch",
+      ш: "sh",
+      щ: "sch",
+      ь: "",
+      ы: "y",
+      ъ: "",
+      э: "e",
+      ю: "yu",
+      я: "ya",
+
+      А: "A",
+      Б: "B",
+      В: "V",
+      Г: "G",
+      Д: "D",
+      Е: "E",
+      Ё: "E",
+      Ж: "Zh",
+      З: "Z",
+      И: "I",
+      Й: "Y",
+      К: "K",
+      Л: "L",
+      М: "M",
+      Н: "N",
+      О: "O",
+      П: "P",
+      Р: "R",
+      С: "S",
+      Т: "T",
+      У: "U",
+      Ф: "F",
+      Х: "H",
+      Ц: "C",
+      Ч: "Ch",
+      Ш: "Sh",
+      Щ: "Sch",
+      Ь: "",
+      Ы: "Y",
+      Ъ: "",
+      Э: "E",
+      Ю: "Yu",
+      Я: "Ya",
     };
-   
-    for (var i = 0; i < word.length; ++i ) {
-      if (converter[word[i]] == undefined){
+
+    for (var i = 0; i < word.length; ++i) {
+      if (converter[word[i]] == undefined) {
         answer += word[i];
       } else {
         answer += converter[word[i]];
       }
     }
-   
+
     return answer;
   }
-  
-  let filtredOrders = orders
-  const [value, setValue] = useState('')
-  if(value) { 
-    function helpMePlease(item) {  
-   for (let i = 0; i < keyCl.length; i++) {
-     if (!item.hasOwnProperty(keyCl[i])) item[keyCl[i]] = ''
-   }
-   if (item.number.toLowerCase().includes(value.toLowerCase().trim())) return true
-   if (item.client.toLowerCase().includes(value.toLowerCase().trim())) return true
-   if (item.status.toLowerCase().includes(value.toLowerCase().trim())) return true
-   if (item.creator.toLowerCase().includes(value.toLowerCase().trim())) return true
 
-   if (translit(item.number.toLowerCase()).includes(value.toLowerCase().trim())) return true
-   if (translit(item.client.toLowerCase()).includes(value.toLowerCase().trim())) return true
-   if (translit(item.status.toLowerCase()).includes(value.toLowerCase().trim())) return true
-   if (translit(item.creator.toLowerCase()).includes(value.toLowerCase().trim())) return true
-      }   
-    filtredOrders = orders.filter(helpMePlease)
-    
+  let filtredOrders = orders;
+  const [value, setValue] = useState("");
+  if (value) {
+    function helpMePlease(item) {
+      for (let i = 0; i < keyCl.length; i++) {
+        if (!item.hasOwnProperty(keyCl[i])) item[keyCl[i]] = "";
+      }
+      if (item.number.toLowerCase().includes(value.toLowerCase().trim()))
+        return true;
+      if (item.client.toLowerCase().includes(value.toLowerCase().trim()))
+        return true;
+      if (item.status.toLowerCase().includes(value.toLowerCase().trim()))
+        return true;
+      if (item.creator.toLowerCase().includes(value.toLowerCase().trim()))
+        return true;
+
+      if (
+        translit(item.number.toLowerCase()).includes(value.toLowerCase().trim())
+      )
+        return true;
+      if (
+        translit(item.client.toLowerCase()).includes(value.toLowerCase().trim())
+      )
+        return true;
+      if (
+        translit(item.status.toLowerCase()).includes(value.toLowerCase().trim())
+      )
+        return true;
+      if (
+        translit(item.creator.toLowerCase()).includes(
+          value.toLowerCase().trim()
+        )
+      )
+        return true;
+    }
+    filtredOrders = orders.filter(helpMePlease);
   }
 
   const clearInput = () => {
@@ -82,7 +151,7 @@ export default function AllOrders() {
       <h1>Все заказы</h1>
 
       <form onSubmit={() => clearInput()} className="search_form">
-      <input
+        <input
           onChange={(event) => setValue(event.target.value)}
           type="text"
           placeholder="Поиск заказа..."
@@ -102,11 +171,12 @@ export default function AllOrders() {
             <ul>
               {filtredOrders?.map((or) => (
                 <Link to={`/orders/${or._id}`}>
-                <div>
-                <h7 key={or._id} >
-                  {or.number} {or.client} {or.status} {or.creator}
-                </h7>
-                </div>
+                  <div>
+                    <h7 key={or._id}>
+                      {or.number} {or.typeFurn} {or.client} {or.status}{" "}
+                      {or.creator}
+                    </h7>
+                  </div>
                 </Link>
               ))}
             </ul>
